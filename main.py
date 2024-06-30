1ba7d8f8250b29f484f4c88a1b8e8d003e5d1384
MapleWithered <wangsj2000@126.com>
2024-06-30 16:31:07 +0800

    fix: main launcher

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
 from arona import startup, momotalk, cafe, tasks, mail, battle, group, alchemy, schedule
 
 if __name__ == '__main__':
-    # # demoviewer.start_viewer()
-    # startup.run_startup()
-    #
-    # # Tasks independent to rewards given
-    # group.run_group()     # ok
-    # battle.run_wanted()   # ok
-    # battle.run_communication()    # ok
-    # cafe.run_cafe()       # ok
-    # # TODO: when list student already in cafe, run second priority or the last one
-    schedule.run_schedule()   # ok
-    momotalk.run_momotalk()   # ok
-    mail.run_mail()                   # ok
-    tasks.run_tasks()                 # ok
+    # demoviewer.start_viewer()
+    startup.run_startup()
+    tasks.run_tasks()
 
-    battle.run_competition()  # ok
+    # Tasks independent to rewards given
+    group.run_group()
+    battle.run_wanted()
+    battle.run_communication()
+    cafe.run_cafe()
+    # TODO: when list student already in cafe, run second priority or the last one
+    schedule.run_schedule()
+    momotalk.run_momotalk()
+    mail.run_mail()
+    tasks.run_tasks()
+
+    battle.run_competition()
 
     # TODO: not implemented
     # tasks.recover_ap()
     # battle.run_task() total 20
     # shop.buy_daily()
 
-    alchemy.run_alchemy()     # ok
-    tasks.run_tasks()       # ok
+    alchemy.run_alchemy()
+    tasks.run_tasks()
 
     # startup.stop_activity()
